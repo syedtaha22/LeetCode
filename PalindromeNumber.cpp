8d0a473c384affc43a754e4598067b87f85e26b1
Syed Taha <syetaha@gmail.com>
2024-04-06 00:47:11 +0500

    9. Palindrome Number

---
+++ PalindromeNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x == 0) return true; 
+        if(x < 0) return false;
+        int n = x;
+        long long flipped = 0;
+        while (n > 0){
+            flipped = flipped * 10 + (n % 10);
+            n /= 10;
+        }
+        return flipped == x;
+    }
+};
+
+int main() {}
