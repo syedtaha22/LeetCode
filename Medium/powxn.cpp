4eceb9b59a242a90371cbbcba5a6763c9b279568
Syed Taha <syetaha@gmail.com>
2024-04-13 16:52:51 +0500

    50. Pow(x, n)

---
+++ Medium/powxn.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n == -1) return 1 / x;
+
+        double half = myPow(x, n / 2);
+
+        if (n % 2) return (n > 0) ? (half * half * x) : (half * half / x);
+        return half * half;
+    }
+};
+
+
+int main(){}
