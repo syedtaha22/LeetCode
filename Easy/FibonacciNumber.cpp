cf222efdadd2824b353764c1e9fd93faae8b75a2
Syed Taha <syetaha@gmail.com>
2024-04-13 16:17:12 +0500

    509. Fibonacci Number

---
+++ Easy/FibonacciNumber.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1; 
+        std::vector<int> fibSequence{0,1};
+        for(int i = 2; i <= n; i++){
+            fibSequence.push_back(fibSequence[i - 1] + fibSequence[i - 2]);
+        }
+        return fibSequence[n];
+    }
+};
+
+
+int main(){}
