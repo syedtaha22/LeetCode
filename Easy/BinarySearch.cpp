7026f94bb5f4da6607cf5404691a5194e1addd3c
Syed Taha <syetaha@gmail.com>
2024-04-26 21:52:36 +0500

    704. Binary Search

---
+++ Easy/BinarySearch.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid = -1;
+        while (left <= right){
+            mid = (right + left) / 2;
+            if(nums[mid] < target) left = mid + 1;
+            else if(nums[mid] > target) right = mid - 1;
+            else return mid;
+        }
+        return -1;        
+    }
+};
+
+
+int main(){}
