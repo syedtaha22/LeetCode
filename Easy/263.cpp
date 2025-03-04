489881d085893e0c3a8ad722037edcff4c4ca5da
Syed Taha <syetaha@gmail.com>
2025-03-04 05:49:32 +0500

    LeetCode 263. Ugly Number

--- Easy/263.cpp
+++ Easy/263.cpp
@@ -9,9 +9,10 @@ class Solution {
 public:
     bool isUgly(int n) {
         if (n <= 0) return false;
-        while (n % 2 == 0) n /= 2;
-        while (n % 3 == 0) n /= 3;
-        while (n % 5 == 0) n /= 5;
+        const int primes[] = { 2, 3, 5 };
+
+        for (int p : primes) while (n % p == 0) n /= p;
+
         return n == 1;
     }
 };
